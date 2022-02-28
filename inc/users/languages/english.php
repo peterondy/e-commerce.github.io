ed9b727247eb952d16e7696c3000b14710e56fa5
Peter Ondy <90659432+peterondy@users.noreply.github.com>
2022-02-28 10:07:22 +0100

    Add files via upload

---
+++ inc/users/languages/english.php
@@ -0,0 +1,129 @@
+<?php  
+//header lang 
+$header = array(
+	'websiteTitle' 		=> 'User Dashboard',
+	'showToolsButton' 	=> 'Show Tools',
+	'tools' 			=> 'Tools',
+	'goToDashboard' 	=> 'Go To Dashboard',
+	'goToNotifications' 	=> 'Go To Notifications',
+	'goToStore' 		=> 'Go To Store',
+	'goToBlog' 			=> 'Go To Blog',
+	'goToSettings' 		=> 'Go To Settings'
+);
+
+//dashboard translate
+$dashboard = array(
+	'pageNotFound' => 'The Page Not Found , ',
+	'redirect' => 'You Will Be Directed After 5 Seconds',
+	'welcome' => 'welcome',
+	'notifications' => 'Notifications',
+	'store' => 'Store',
+	'blog' => 'Blog',
+	'view' => 'View',
+	'settings' => 'Settings',
+	'allTransactions' => 'All Transactions',
+	'userID' => '# User ID',
+	'itemNumber' => 'Item Number',
+	'price' => 'Price',
+	'delete' => 'Delete',
+);
+
+//blog translate
+$blog = array(
+	'showTheBlogs' => 'Show Your Blogs',
+	'pageNotFound' => 'The Page Not Found , ',
+	'userNotFound' => 'The User Not Found',
+	'redirect' => 'You Will Be Directed After 5 Seconds',
+	'viewPage' => 'View Page',
+	'view' => 'View',
+	'store' => 'Store',
+	'blog' => 'Blog',
+	'settings' => 'Settings',
+	'allTransactions' => 'All Transactions',
+	'userID' => '# User ID',
+	'itemNumber' => 'Item Number',
+	'price' => 'Price',
+	'delete' => 'Delete',
+);
+
+//store translate
+$store = array(
+	'showTheStore' => 'Show The Store',
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//pricing translate
+$pricing = array(
+	'header' => 'PRICING',
+	'freeHeader' => 'FREE',
+	'plusHeader' => 'PLUS+',
+	'proHeader' => 'PRO',
+	'proPlusHeader' => 'PRO PLUS +',
+	'freeLogo' => 'FREE',
+	'plusLogo' => 'PLUS+',
+	'proLogo' => 'PRO',
+	'proPlusLogo' => 'PRO PLUS +',
+	'freeDesc' => 'A Free Account',
+	'plusDesc' => 'A Free Account',
+	'proDesc' => 'A Free Account',
+	'proDescPlus' => 'A Free Account',
+);
+
+//shop translate
+$shop = array(
+	'header' => 'SHOP',
+);
+
+//about translate
+$about = array(
+	'header' => 'About The Website',
+	'desc' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+	'aboutInfos' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+	'btn' => 'learn more'
+);
+
+//contact translate
+$contact = array(
+	'header' => 'About The Website',
+	'desc' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+	'aboutInfos' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+	'btn' => 'learn more'
+);
+
+//footer translate
+$footer = array(
+	'header' => '© Copyright Reserved ',
+	'link' => 'ECommerce'
+);
+
+//login translate
+$login = array(
+	'header' 				=> ' Login ',
+	'loginPage' 			=> 'Login Page',
+	'signIn' 				=> 'Sign In',
+	'login' 				=> 'Login',
+	'adminLogin'			=> 'Admin Login',
+	'loginFromHeader' 		=> 'Login In Page',
+	'loginFromEmail' 		=> 'Email',
+	'loginFromUsernamer' 	=> 'Username',
+	'loginFromPassword' 	=> 'Password',
+	'signInFromHeader' 		=> 'Sign In Page',
+	'confirmPassFromHeader' => 'Confirm Password',
+	'adminLoginFromHeader' 	=> 'Admin Login Page',
+	'go' => 'GO'
+);
+
+
+?>
