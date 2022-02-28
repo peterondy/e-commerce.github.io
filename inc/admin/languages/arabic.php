5e6acdbb7985a0f70f76ea11613022dc0b4b5608
Peter Ondy <90659432+peterondy@users.noreply.github.com>
2022-02-28 10:04:13 +0100

    Create arabic.php

---
+++ inc/admin/languages/arabic.php
@@ -0,0 +1,96 @@
+ <?php 
+//header lang 
+$header = array(
+	'language' => 'ar',
+	'websiteTitle' => 'لوحة تحكم المشرف ',
+	'brand' => 'متجر الكتروني  ',
+	'btn' => ' إظهار قائمة الأدوات الجانبية ',
+	'leftTool' => 'القائمة الجانبية ',
+	'pages' => 'الصفحات  ',
+	'stats' => 'الإحصاءات ',
+	'profile' => 'الملف الشخصي ',
+	'store' => 'المتجر',
+	'blog' => 'المدونة',
+	'settings' => 'الإعدادات '
+);
+
+ 
+//dashboard translate
+$dashboard = array(
+	'h1' => 'مرحبا ',
+	'rows' => 'سطور ',
+	'infos' => 'معلومات ',
+	'url\'s' => 'روابط ',
+	'members' => 'أعضاء ',
+	'signed' => 'عضو جديد ',
+	'view' => 'الإنتقال الى الصفحة ',
+	'items' => 'سلع ',
+	'buyed' => 'تم شراؤها ',
+	'contacts' => 'الإتصال',
+);
+
+
+//page translate
+$page = array(
+	'members' 	 => 'الأعضاء ',
+	'id' 	  	 => 'معرف ',
+	'email' 	 => 'البريد الإلكتروني ',
+	'userName'	 => 'إسم المستخدم ',
+	'status' 	 => 'الحالة ',
+	'links' 	 => 'الروابط ',
+	'notAcivate' => 'غير مفعل ',
+	'activate' => 'تفعيل ',
+	'delete' => 'حذف ',
+	'notFound' => 'معرف المستخدم غير موجود ',
+	'redirect' => 'سيتم اعادتك الى الصفحة السابقة ',
+	'withID' => 'المستخدم  الذي معرفه ',
+	'isactivated' => 'تم تفعيله ',
+	'isdeleted' => 'نم حذفه '
+);
+
+
+
+//footer translate
+$footer = array(
+	'header' => '      جميع الحقوق محفوظة  © ',
+	'link' => ' المتجر الإلكتروني  '
+);
+
+
+//settings translate
+$settings = array(
+	'email' => 'البريد الإلكتروني ',
+	'userName' => 'إسم المستخدم ',
+	'password' => 'كلمة المرور ',
+	'oldPassword' => 'كلمة المرور القديمة ',
+	'button' => 'حفظ '
+);
+
+//blogs translate
+$blogs = array(
+	'page' 	 		 => 'التدوينات ',
+	'add' 	 		 => 'إضافة تدوينة جديدة ',
+	'img' 	 		 => 'الصور ',
+	'title'  		 => 'العنوان ',
+	'desc'  		 => 'الوصف ',
+	'image'  		 => 'تحميل صورة ',
+	'date' 	 		 => 'التاريخ ',
+	'author' 		 => 'الكاتب ',
+	'id' 	 		 => 'معرف التدوينة ',
+	'links'  		 => 'الروابط ',
+	'view' 	 		 => 'مشاهدة ',
+	'desactivate' 	 => 'تعطيل ',
+	'activate' 	 	 => 'تفعيل ',
+	'edit' 	 		 => 'تعديل ',
+	'delete' 		 => 'حذف ',
+	'redirect' 		 => 'إعادة توجيه  ',
+	'withID' 		 => 'ذو المعرف ',
+	'notFound' 		 => 'الصفحة غير موجودة ',
+	'view' 		 	 => 'صفحة المشاهدة ',
+	'edit' 		 => 'صفحة التعديل ',
+	'insert'		 => 'تمت إضافة التدوينة  '
+);
+
+
+
+?>
