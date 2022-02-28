97e10c36f94549df28c84690cee26d08fbe8909d
Peter Ondy <90659432+peterondy@users.noreply.github.com>
2022-02-28 10:04:51 +0100

    Add files via upload

---
+++ inc/admin/languages/english.php
@@ -0,0 +1,114 @@
+<?php 
+//header lang 
+$header = array(
+	'language' 		=> 'en',
+	'websiteTitle'  => 'Admin Dashboard',
+	'brand' 		=> 'Ecommerce',
+	'btn' 			=> 'Show Left Tools Bar',
+	'leftTool' 		=> 'Left Tools Bar',
+	'pages' 		=> 'Pages',
+	'stats' 		=> 'Stats',
+	'profile' 		=> 'Profile',
+	'store' 		=> 'Store',
+	'blog' 			=> 'Blog',
+	'settings' 		=> 'Settings'
+);
+
+//dashboard translate
+$dashboard = array(
+	'h1' 		=> 'Welcome',
+	'rows' 		=> 'Rows',
+	'infos' 	=> 'Infos',
+	'url\'s' 	=> 'URL\'S',
+	'members' 	=> 'Members',
+	'signed' 	=> 'Signed',
+	'view' 		=> 'View Page',
+	'items' 	=> 'Items',
+	'buyed' 	=> 'Buyed',
+	'contacts' 	=> 'Contacts',
+);
+
+//page translate
+$page = array(
+	'members' 	  => 'Members',
+	'id' 	  	  => 'ID',
+	'email' 	  => 'Email',
+	'userName'	  => 'User Name',
+	'status' 	  => 'Status',
+	'links' 	  => 'Links',
+	'notAcivate'  => 'Not Activated',
+	'activate' 	  => 'Activate',
+	'delete' 	  => 'Delete',
+	'notFound' 	  => 'The User ID Not Found',
+	'redirect' 	  => 'You will be directed after 5 minutes',
+	'withID' 	  => 'The User With ID',
+	'isactivated' => 'IS Activated',
+	'isdeleted'   => 'IS Deleted'
+);
+
+//footer translate
+$footer = array(
+	'header' => '© Copyright Reserved ',
+	'link' 	 => 'ECommerce'
+);
+
+//settings translate
+$settings = array(
+	'email' 	  => 'Email',
+	'userName' 	  => 'User Name',
+	'password' 	  => 'Password',
+	'oldPassword' => 'OLD Password',
+	'button' 	  => 'Save'
+);
+
+//blogs translate
+$blogs = array(
+	'page' 	 		 => 'Blogs',
+	'add' 	 		 => 'Add New Blog',
+	'img' 	 		 => 'Images',
+	'title'  		 => 'Title',
+	'desc'  		 => 'Description',
+	'image'  		 => 'Upload Image',
+	'date' 	 		 => 'Date',
+	'author' 		 => 'Author',
+	'id' 	 		 => 'ID',
+	'links'  		 => 'Links',
+	'view' 	 		 => 'View',
+	'desactivate' 	 => 'Desactivate',
+	'activate' 	 	 => 'Activate',
+	'edit' 	 		 => 'Edit',
+	'delete' 		 => 'Delete',
+	'redirect' 		 => 'Redirect',
+	'withID' 		 => 'With ID',
+	'notFound' 		 => 'Page Not Found',
+	'view' 		 	 => 'View Page',
+	'edit' 		 	 => 'Edit Page',
+	'notFound' 		 => 'Page Not Found',
+	'insert'		 => 'The Blog Are Inserted'
+);
+
+//store translate
+$store = array(
+	"showTools" 	=> "Show Tools",
+	"tools" 		=> "Tools",
+	"store" 		=> "Show Your Store",
+	"add" 			=> "Add New Item To Your Store",
+	"id" 			=> "ID",
+	"name" 			=> "Name",
+	"description" 	=> "Description",
+	"date" 			=> "Date",
+	"links" 		=> "Links",
+	"view" 			=> "View",
+	"name" 			=> "Name",
+	"name" 			=> "Name",
+	"name" 			=> "Name",
+	"name" 			=> "Name",
+	"name" 			=> "Name",
+	"name" 			=> "Name",
+	"name" 			=> "Name",
+	"name" 			=> "Name",
+	"name" 			=> "Name",
+	"name"			=> "Name",
+);
+
+?>
