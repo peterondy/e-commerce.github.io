168f16d131a97f95112df88366d1e8031b4033b7
Peter Ondy <90659432+peterondy@users.noreply.github.com>
2022-02-28 10:07:05 +0100

    Create arabic.php

---
+++ inc/users/languages/arabic.php
@@ -0,0 +1,57 @@
+<?php  
+//header lang 
+$header = array(
+	'websiteTitle' 		=> 'لوحة تحكم المستخدم',
+	'showToolsButton' 	=> 'إظهار الأدوات ',
+	'tools' 			=> 'الأدوات ',
+	'goToDashboard' 	=> 'الذهاب الى الصفحة الرئيسية ',
+	'goToNotifications' 	=> 'الذهاب إلى الإشعارات ',
+	'goToStore' 		=> 'الذهاب إالى المتجر  ',
+	'goToBlog' 			=> 'الذهاب إلى المدونة ',
+	'goToSettings' 		=> 'الذهاب إالى الإعدادات '
+);
+
+//dashboard translate
+$dashboard = array(
+	'pageNotFound' => 'الصفحة غير موجودة , ',
+	'redirect' => 'سيتم إعادة تحويلك بعد 5 ثوان ',
+	'welcome' => 'أهلا ',
+	'notifications' => 'الإشعارات ',
+	'store' => 'المتجر ',
+	'blog' => 'المدونة ',
+	'view' => 'مشادة ',
+	'settings' => 'الإعدادات ',
+	'allTransactions' => 'كل المعاملات ',
+	'userID' => 'معرف المستخدم ',
+	'itemNumber' => 'رقم العنصر ',
+	'price' => 'السعر',
+	'delete' => 'حذف',
+);
+
+//blog translate
+$blog = array(
+	'showTheBlogs' => 'Show Your Blogs',
+	'pageNotFound' => 'The Page Not Found , ',
+	'userNotFound' => 'The User Not Found',
+	'redirect' => 'You Will Be Directed After 5 Seconds',
+	'viewPage' => 'View Page',
+	'view' => 'View',
+	'store' => 'Store',
+	'blog' => 'Blog',
+	'settings' => 'Settings',
+	'allTransactions' => 'All Transactions',
+	'userID' => '# User ID',
+	'itemNumber' => 'Item Number',
+	'price' => 'Price',
+	'delete' => 'Delete',
+);
+
+//store translate
+$store = array(
+	'showTheStore' => 'إظهار المتجر',
+);
+
+$footer = array(
+	'header' => '  كل الحقوق محفوظة © ',
+	'link' => 'متجر الكتروني'
+);
